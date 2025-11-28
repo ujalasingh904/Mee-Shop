4f4a92ad93a2f46e335693a9ca901b96e5eeebd2
ujala <singhujala2004@gmail.com>
2025-11-28 19:31:13 +0530

    chore: update API_URL to use environment variable and configure CORS origin

--- server/index.js
+++ server/index.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: process.env.VITE_API_URL || 'http://localhost:5173',
+  }
+));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
