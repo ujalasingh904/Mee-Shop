c3d789f3cf8b5b13e1bdbe53a21710db51fac188
ujala <singhujala2004@gmail.com>
2025-11-28 18:51:54 +0530

    My local changes

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -4,13 +4,13 @@ import DarkButton from '../../assets/website/dark-mode-button.png'
 
 const DarkMode = () => {
     const[theme,setTheme] = useState(localStorage.getItem("theme")?localStorage.getItem("theme"):"light");
+
     const element = document.documentElement;
     
     useEffect(()=> {
         localStorage.setItem("theme",theme);
         if(theme === 'dark'){ 
-            element.classList.add("dark")
-            element.classList.add("dark")
+            element.classList.add("dark") 
         }else{
             element.classList.remove('light');
             element.classList.remove('dark');
